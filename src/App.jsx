76b2991dfb0390b14067d824b9718469738bd663
Sergi0let <s.vashkevych@gmail.com>
2022-11-09 21:51:14 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import HeaderContainer from './components/Header/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
-import ProfileContainer from './components/Profile/ProfileContainer';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import UsersContainer from './components/Users/UsersContainer';
 import Login from './components/Login/Login';
 
-import './App.scss';
-import { connect } from 'react-redux';
 import { initialize } from './redux/appReducer';
 import Preloader from './components/common/preloader/Preloader';
 
+import './App.scss';
+
+const DialogsContainer = React.lazy(() =>
+  import('./components/Dialogs/DialogsContainer')
+);
+const ProfileContainer = React.lazy(() =>
+  import('./components/Profile/ProfileContainer')
+);
 class App extends React.Component {
   componentDidMount() {
     this.props.initialize();
@@ -34,23 +39,35 @@ class App extends React.Component {
             <div className="app__content content">
               <div className="content__img-bg"></div>
               <Routes>
-                <Route path="/profile" element={<ProfileContainer />} />
+                <Route
+                  path="/profile"
+                  element={
+                    <Suspense fallback={<div>Loading...</div>}>
+                      <ProfileContainer />
+                    </Suspense>
+                  }
+                />
                 <Route
                   path="/profile/:profileId"
                   element={
-                    <ProfileContainer
-                      dispatch={this.props.dispatch}
-                      store={this.props.store}
-                    />
+                    <Suspense fallback={<div>Loading...</div>}>
+                      <ProfileContainer
+                        dispatch={this.props.dispatch}
+                        store={this.props.store}
+                      />
+                    </Suspense>
                   }
                 />
                 <Route
                   path="/dialogs/*"
                   element={
-                    <DialogsContainer
-                      store={this.props.store}
-                      dispatch={this.props.dispatch}
-                    />
+                    <Suspense fallback={<div>Loading...</div>}>
+                      {' '}
+                      <DialogsContainer
+                        store={this.props.store}
+                        dispatch={this.props.dispatch}
+                      />
+                    </Suspense>
                   }
                 />
                 <Route path="/users/*" element={<UsersContainer />} />
