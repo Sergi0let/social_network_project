e32966196ecc8729cc93d1924409dbc71cdfd36b
Sergi0let <s.vashkevych@gmail.com>
2022-11-21 11:45:10 +0200

    init

---
+++ src/redux/usersSelectors.ts
@@ -0,0 +1,13 @@
+// @ts-ignore
+import { AppStateType } from './store-redux.ts';
+
+export const getUsersSelector = (state: AppStateType) => state.usersPage.users;
+export const getPageSize = (state: AppStateType) => state.usersPage.pageSize;
+export const getTotalUsersCount = (state: AppStateType) =>
+  state.usersPage.totalUsersCount;
+export const getCurrentPage = (state: AppStateType) =>
+  state.usersPage.currentPage;
+export const getIsFetching = (state: AppStateType) =>
+  state.usersPage.isFetching;
+export const getFollowingInProgress = (state: AppStateType) =>
+  state.usersPage.followingInProgress;
